870ca9cf4baf02a4b62f3e99231cd06f03117eb0
sukanda237 <sukandawiguna@mail.unpas.ac.id>
2024-08-07 08:56:51 +0700

    memberikan komentar pada m siswa

--- models/m_siswa.php
+++ models/m_siswa.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . "../../controllers/c_koneksi.php");
 class siswa
 {
 
-	//ini untuk menampilkan data dari tabel siswa
+	//fungsi untuk menampilkan semua data dari tabel siswa
 	function tampil_data()
 	{
 
@@ -26,37 +26,67 @@ class siswa
 		return $hasil;
 	}
 
+	// fungsi menampilkan data berdasarkan id yang dipilih
 	function tampil_data_id($id)
 	{
+		//membuka koneksi database
 		$conn = new database();
+
+		// menampilkan data siswa berdasarkan id siswa yang dipilih
 		$query = mysqli_query($conn->koneksi, "SELECT * FROM siswa WHERE id_siswa = $id");
+
+		//ubah data menjadi objeck 
 		while ($row = mysqli_fetch_object($query)) {
+			//simpan kedalam variabel hasil yang berbentuk array
 			$hasil[] = $row;
 		}
 
+		//kembalikan nilai nya
 		return $hasil;
 	}
 
+	//fungsi untuk memasukan data kedalam tabel siswa
 	function input($id, $nama, $jk, $alamat)
 	{
+		//membuka koneksi database
 		$conn = new database();
+
+		//perintah untuk memasukan kedalam tabel siswa
 		$sql = "INSERT INTO siswa VALUES ('$id','$nama', '$jk', '$alamat')";
+
+		//mengeksekusi perintah diatas
 		$result = mysqli_query($conn->koneksi, $sql);
+
+		//mengembalikan nilai atau pesan bahwa data berhasil dimasukan
 		return $result;
 	}
 
+	// fungsi untuk mengubah data
 	function update($id, $nama, $jk, $alamat)
 	{
+		//membuka koneksi database
 		$conn = new database();
+
+		//perintah untuk mengubah data pada tabel siswa
 		$query = mysqli_query($conn->koneksi, "UPDATE siswa SET nama_siswa='$nama', jk_siswa='$jk', alamat_siswa = '$alamat'  WHERE id_siswa = $id");
+
+		//mengembalikan nilai atau pesan bahwa data berhasil diubah
 		return $query;
 	}
 
+	//fungsi untuk menghapus data 
 	function hapus($id)
 	{
+		//membuka koneksi database
 		$conn = new database();
+
+		//perintah untuk menghapus data sesuai dengan id siswa yang dipilih
 		$sql = "DELETE FROM siswa WHERE id_siswa = $id";
+
+		//mengeksekusi perintah diatas
 		$result = mysqli_query($conn->koneksi, $sql);
+
+		//mengembalikan nilai atau pesan bahwa data berhasil diubah
 		return $result;
 	}
 }
